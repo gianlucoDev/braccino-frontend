2ff35d96c7dcc410a7e960f982760fe722166929
darcros <darcros7@gmail.com>
2021-04-23 21:56:22 +0200

    add Material-UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import useSWR from 'swr';
 
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 const BASE_URL = 'http://localhost:8000';
 
 const fetcher = (url, ...args) =>
@@ -12,10 +18,17 @@ function App() {
   if (!data) return <p>Loading...</p>;
 
   return data.map(({ id, name, serial }) => (
-    <div key={id}>
-      <p>Name: {name}</p>
-      <p>Serial: {serial}</p>
-    </div>
+    <Card key={id}>
+      <CardContent>
+        <Typography>Name: {name}</Typography>
+        <Typography>Serial: {serial}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={() => alert('Non ancora impementato')}>
+          Attiva
+        </Button>
+      </CardActions>
+    </Card>
   ));
 }
 
