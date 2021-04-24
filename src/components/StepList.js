9f28b69583d65e61c27c26ad9e6398069edcc839
darcros <darcros7@gmail.com>
2021-04-24 17:40:57 +0200

    improve display of steps in list

--- src/components/StepList.js
+++ src/components/StepList.js
@@ -1,36 +1,23 @@
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
-import IconButton from '@material-ui/core/IconButton';
+import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
 
-import EditIcon from '@material-ui/icons/Edit';
 import AddIcon from '@material-ui/icons/Add';
+import StepListItem from './StepListItem';
 
 function StepList({ steps, onEdit, onAdd }) {
   return (
     <List>
       {steps.map((step, i) => (
-        <ListItem key={i}>
-          <ListItemAvatar>
-            <Avatar>{i + 1}</Avatar>
-          </ListItemAvatar>
-          <ListItemText
-            primary={`Delay: ${step.delay}`}
-            secondary={`m1 ${step.m1}°, m2 ${step.m2}°, m3 ${step.m3}°, m4 ${step.m4}°, m5 ${step.m5}°, m6 ${step.m6}°`}
-          />
-          <ListItemSecondaryAction>
-            <IconButton edge="end" aria-label="edit" onClick={onEdit}>
-              <EditIcon />
-            </IconButton>
-          </ListItemSecondaryAction>
-        </ListItem>
+        <StepListItem key={i} index={i} step={step} onEdit={() => onEdit(i)} />
       ))}
 
-      <ListItem inset>
+      <Divider />
+
+      <ListItem alignItems="flex-start">
         <ListItemText primary="Nuovo step" />
         <ListItemSecondaryAction>
           <Button onClick={onAdd} startIcon={<AddIcon />}>
---
+++ src/components/StepListItem.js
@@ -0,0 +1,65 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
+import Avatar from '@material-ui/core/Avatar';
+
+import Chip from '@material-ui/core/Chip';
+
+import EditIcon from '@material-ui/icons/Edit';
+
+const useStyles = makeStyles((theme) => ({
+  valuesContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(0.25),
+    },
+  },
+  avatar: {
+    color: theme.palette.getContrastText(theme.palette.primary.dark),
+    backgroundColor: theme.palette.primary.dark,
+  },
+}));
+
+function StepListItem({ index, step, onEdit }) {
+  const classes = useStyles();
+
+  const values = (
+    <div className={classes.valuesContainer}>
+      <Chip size="small" avatar={<Avatar>1</Avatar>} label={step.m1 + '°'} />
+      <Chip size="small" avatar={<Avatar>2</Avatar>} label={step.m2 + '°'} />
+      <Chip size="small" avatar={<Avatar>3</Avatar>} label={step.m3 + '°'} />
+      <Chip size="small" avatar={<Avatar>4</Avatar>} label={step.m4 + '°'} />
+      <Chip size="small" avatar={<Avatar>5</Avatar>} label={step.m5 + '°'} />
+      <Chip size="small" avatar={<Avatar>6</Avatar>} label={step.m6 + '°'} />
+    </div>
+  );
+
+  return (
+    <ListItem alignItems="flex-start">
+      <ListItemAvatar>
+        <Avatar variant="rounded" className={classes.avatar}>
+          {index + 1}
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText
+        primary={`Delay: ${step.delay}`}
+        primaryTypographyProps={{ variant: 'h6' }}
+        secondary={values}
+        // HACK: I actually use the second <Typography> as a <div> to hold the values
+        secondaryTypographyProps={{ component: 'div' }}
+      />
+      <ListItemSecondaryAction>
+        <IconButton edge="end" aria-label="edit" onClick={onEdit}>
+          <EditIcon />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
+}
+
+export default StepListItem;
