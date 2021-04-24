de0e681f76f4b0d60599004dc18da4e58367a701
darcros <darcros7@gmail.com>
2021-04-24 13:53:14 +0200

    list routines in main page

---
+++ src/components/RoutineList.js
@@ -0,0 +1,38 @@
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+function RoutineList({ routines }) {
+  return (
+    <GridList cellHeight="auto" cols={3}>
+      {routines.map(({ id, name, steps }) => (
+        <GridListTile key={id}>
+          <Card>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {name}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Steps: {steps.length}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button
+                size="small"
+                onClick={() => alert('Non ancora impementato')}
+              >
+                Modifica
+              </Button>
+            </CardActions>
+          </Card>
+        </GridListTile>
+      ))}
+    </GridList>
+  );
+}
+
+export default RoutineList;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,49 +1,27 @@
 import useSWR from 'swr';
 
-import GridList from '@material-ui/core/GridList';
-import Box from '@material-ui/core/Box';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import BraccioList from '../components/BraccioList';
+import RoutineList from '../components/RoutineList';
 
 function MainPage() {
-  const { data, error } = useSWR('/braccio/');
+  const { data: braccios, error: bracciosError } = useSWR('/braccio');
+  const { data: routines, error: routinesError } = useSWR('/routines');
 
-  if (error) return <p>Error.</p>;
-  if (!data) return <p>Loading...</p>;
+  if (bracciosError || routinesError) return <p>Error.</p>;
+  if (!braccios || !routines) return <p>Loading...</p>;
 
   return (
     <>
       <Typography gutterBottom variant="h3" component="h1">
         Bracci collegati
       </Typography>
+      <BraccioList braccios={braccios} />
 
-      <GridList cellHeight="auto" cols={3}>
-        {data.map(({ id, name, serial }) => (
-          <Box key={id} m={1}>
-            <Card>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {name}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Serial: {serial}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  size="small"
-                  onClick={() => alert('Non ancora impementato')}
-                >
-                  Attiva
-                </Button>
-              </CardActions>
-            </Card>
-          </Box>
-        ))}
-      </GridList>
+      <Typography gutterBottom variant="h3" component="h1">
+        Routine disponibili
+      </Typography>
+      <RoutineList routines={routines} />
     </>
   );
 }
