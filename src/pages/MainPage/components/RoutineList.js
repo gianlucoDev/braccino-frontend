6b72486350e8ee7c1aa28122aa3086b918bea4ab
darcros <darcros7@gmail.com>
2021-05-04 17:36:15 +0200

    merge CardButton back into RoutineList link card button to new routine page

--- src/pages/MainPage/components/RoutineList.js
+++ src/pages/MainPage/components/RoutineList.js
@@ -1,15 +1,17 @@
 import { Link as RouterLink } from 'react-router-dom';
 
 import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '@material-ui/core/Typography';
 
-import CardButton from './CardButton';
+import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles((theme) => ({
   full: {
@@ -48,10 +50,24 @@ function RoutineList({ routines }) {
       ))}
 
       <GridListTile>
-        <CardButton
-          text="Aggiungi una routine"
-          onClick={() => alert('Non ancora implementato')}
-        />
+        <Card className={classes.full}>
+          <ButtonBase
+            className={classes.full}
+            component={RouterLink}
+            to="/routines/new"
+          >
+            <Box
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              height="100%"
+            >
+              <AddIcon style={{ fontSize: '5em' }} />
+              <Typography>Aggiungi una routine</Typography>
+            </Box>
+          </ButtonBase>
+        </Card>
       </GridListTile>
     </GridList>
   );
