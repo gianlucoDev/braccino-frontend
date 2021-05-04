457d85c9f40efd07dcc63446048acc44ef646bda
darcros <darcros7@gmail.com>
2021-05-04 18:01:23 +0200

    implement routine creation page

--- src/pages/RoutinePage/NewRoutinePage.js
+++ src/pages/RoutinePage/NewRoutinePage.js
@@ -1,7 +1,49 @@
-import Typography from '@material-ui/core/Typography';
+import { useHistory } from 'react-router-dom';
+
+import Box from '@material-ui/core/Box';
+
+import useDirtyData from '../../hooks/useDirtyData';
+import RoutineEditor from './components/RoutineEditor';
+
+import { BASE_URL } from '../../fetcher';
+const DEFAULT_ROUTINE = { name: '', steps: [] };
 
 function NewRoutinePage() {
-  return <Typography>Non ancora implementato</Typography>;
+  const history = useHistory();
+  const [state, setState, dirty, reset] = useDirtyData(DEFAULT_ROUTINE);
+
+  const handleSubmit = async () => {
+    const res = await fetch(BASE_URL + `/routines`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(state),
+    });
+    const createdRoutine = await res.json();
+
+    // TODO: validate user input
+    // TODO: handle non-200 responses
+
+    // navigate to newly created routine
+    history.push(`/routines/${createdRoutine.id}`);
+  };
+
+  const handleReset = () => {
+    reset();
+  };
+
+  return (
+    <Box padding={2}>
+      <RoutineEditor
+        routine={state}
+        enableSubmitCancel={dirty}
+        onChange={setState}
+        onSubmit={handleSubmit}
+        onCancel={handleReset}
+      />
+    </Box>
+  );
 }
 
 export default NewRoutinePage;
