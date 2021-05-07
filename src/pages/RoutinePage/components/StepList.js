a75b8ef38bb5e907e4c5c59b9079aa31b0d80364
darcros <darcros7@gmail.com>
2021-05-07 19:57:06 +0200

    removed unused imports

--- src/pages/RoutinePage/components/StepList.js
+++ src/pages/RoutinePage/components/StepList.js
@@ -2,9 +2,6 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import Box from '@material-ui/core/Box';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
