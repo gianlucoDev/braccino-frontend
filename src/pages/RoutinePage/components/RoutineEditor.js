1498caeb27e67b29f6f0aefde92f8bb828f5555c
darcros <darcros7@gmail.com>
2021-05-09 15:23:24 +0200

    tabbed layout for smaller screens

--- src/pages/RoutinePage/components/RoutineEditor.js
+++ src/pages/RoutinePage/components/RoutineEditor.js
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Hidden from '@material-ui/core/Hidden';
 import Typography from '@material-ui/core/Typography';
 
 import RoutineEditorControls from './RoutineEditorControls';
 
 import StepListEditor from './StepListEditor';
+
+const useStyles = makeStyles((theme) => ({
+  tabsPaper: {
+    backgroundColor: theme.palette.primary.main,
+  },
+}));
 
 function RoutineEditor({
   routine,
@@ -21,6 +34,7 @@ function RoutineEditor({
   enableRun = false,
   onDelete,
 }) {
+  const classes = useStyles();
   const nameError = enableSubmit && !routine.name;
 
   const handleNameChange = (name) => {
@@ -30,7 +44,56 @@ function RoutineEditor({
     });
   };
 
-  return (
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const handleTabChange = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
+
+  const tabbedLayout = () => (
+    <>
+      <Paper square elevation={0} className={classes.tabsPaper}>
+        <Tabs
+          variant="fullWidth"
+          value={selectedTab}
+          onChange={handleTabChange}
+        >
+          <Tab label="Routine" />
+          <Tab label="Steps" />
+        </Tabs>
+      </Paper>
+
+      <div hidden={selectedTab !== 0}>
+        <Box padding={2}>
+          <Typography variant="h4" gutterBottom>
+            Routine
+          </Typography>
+
+          <RoutineEditorControls
+            routine={routine}
+            // name text field
+            nameError={nameError}
+            onNameChange={handleNameChange}
+            // submit-cancel actions
+            enableSubmit={enableSubmit}
+            enableCancel={enableCancel}
+            onSubmit={onSubmit}
+            onCancel={onCancel}
+            // optional actions
+            showOptionalActions={showOptionalActions}
+            enableRun={enableRun}
+            onDelete={onDelete}
+          />
+        </Box>
+      </div>
+
+      <div hidden={selectedTab !== 1}>
+        <StepListEditor routine={routine} onChange={onChange} />
+      </div>
+    </>
+  );
+
+  const desktopLayout = () => (
     <Grid container style={{ height: '100%' }}>
       {/* left left column */}
       <Grid item xs={3}>
@@ -61,6 +124,13 @@ function RoutineEditor({
         <StepListEditor routine={routine} onChange={onChange} />
       </Grid>
     </Grid>
+  );
+
+  return (
+    <>
+      <Hidden smDown>{desktopLayout()}</Hidden>
+      <Hidden mdUp>{tabbedLayout()}</Hidden>
+    </>
   );
 }
 
