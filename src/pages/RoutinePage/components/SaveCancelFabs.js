26a335d363dd32dfe8c4e243403e7799a15dc8b6
darcros <darcros7@gmail.com>
2021-05-12 17:13:02 +0200

    show FABs with transition

--- src/pages/RoutinePage/components/SaveCancelFabs.js
+++ src/pages/RoutinePage/components/SaveCancelFabs.js
@@ -1,6 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 import Fab from '@material-ui/core/Fab';
+import Slide from '@material-ui/core/Slide';
 
 import SaveIcon from '@material-ui/icons/Save';
 import CancelIcon from '@material-ui/icons/Cancel';
@@ -19,36 +20,35 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function SaveCancelFabs({
-  disableSubmit,
-  disableCancel,
-  onSubmit,
-  onCancel,
-}) {
+function SaveCancelFabs({ disableSubmit, disableCancel, onSubmit, onCancel }) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Fab
-        variant="extended"
-        size="large"
-        color="primary"
-        disabled={disableSubmit}
-        onClick={onSubmit}
-      >
-        <SaveIcon className={classes.extendedFabIcon} />
-        Salva
-      </Fab>
-      <Fab
-        variant="extended"
-        size="large"
-        color="secondary"
-        disabled={disableCancel}
-        onClick={onCancel}
-      >
-        <CancelIcon className={classes.extendedFabIcon} />
-        Annulla
-      </Fab>
+      <Slide direction="up" in={!disableSubmit}>
+        <Fab
+          variant="extended"
+          size="large"
+          color="primary"
+          disabled={disableSubmit}
+          onClick={onSubmit}
+        >
+          <SaveIcon className={classes.extendedFabIcon} />
+          Salva
+        </Fab>
+      </Slide>
+      <Slide direction="up" in={!disableCancel}>
+        <Fab
+          variant="extended"
+          size="large"
+          color="secondary"
+          disabled={disableCancel}
+          onClick={onCancel}
+        >
+          <CancelIcon className={classes.extendedFabIcon} />
+          Annulla
+        </Fab>
+      </Slide>
     </div>
   );
 }
