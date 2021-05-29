61761ac78e53974a342f0230eff0d339e0fcaee8
darcros <darcros7@gmail.com>
2021-05-29 17:09:13 +0200

    call API on the same hostname the app was loaded from

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,4 +1,7 @@
-const BASE_URL = 'http://localhost:8000';
-const BASE_URL_WS = 'ws://localhost:8000/ws';
+// http://localhost:8000
+const BASE_URL = `http://${window.location.hostname}:8000`;
+
+// ws://localhost:8000/ws
+const BASE_URL_WS = `ws://${window.location.hostname}:8000/ws`;
 
 export { BASE_URL, BASE_URL_WS };
