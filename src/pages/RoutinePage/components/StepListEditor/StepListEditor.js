7949603667fc1b5aaa3c53369568d8166b88ba3f
darcros <darcros7@gmail.com>
2021-06-25 17:48:50 +0200

    merge StepListEditorWide into StepListEditor

--- src/pages/RoutinePage/components/StepListEditor/StepListEditor.js
+++ src/pages/RoutinePage/components/StepListEditor/StepListEditor.js
@@ -1,14 +1,36 @@
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 
-import StepListEditorWide from './StepListEditorWide';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+
+import StepList from '../StepList';
+import StepEditor from '../StepEditor';
 import StepListEditorMobile from './StepListEditorMobile';
 
 function StepListEditor() {
   const theme = useTheme();
-  const sm = useMediaQuery(theme.breakpoints.up('sm'));
+  const mobile = useMediaQuery(theme.breakpoints.only('xs'));
 
-  return sm ? <StepListEditorWide /> : <StepListEditorMobile />;
+  if (mobile) {
+    return <StepListEditorMobile />;
+  }
+
+  return (
+    <Box padding={2}>
+      <Grid container>
+        <Grid item xs={6}>
+          <StepList />
+        </Grid>
+
+        <Grid item xs={6}>
+          <Box height="100%" padding={2}>
+            <StepEditor />
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 export default StepListEditor;
