1b225558fa4af9f026de586772eca06b54793f32
darcros <darcros7@gmail.com>
2021-07-03 12:03:46 +0200

    refactor canvas and cartesianPlane

--- src/components/inputs/PositionInput/Canvas.js
+++ src/components/inputs/PositionInput/Canvas.js
@@ -1,6 +1,23 @@
 import { useEffect, useRef } from 'react';
 
-function Canvas({ draw, onClick, ...props }) {
+/**
+ * Takes a MouseEvent fired from a <canvas> and converts the coordinates of
+ * the MouseEvent in <cavas> coordinates
+ *
+ * @param {MouseEvent} event a MouseEvent that has been fired from a <canvas> element
+ * @return an object with the coordinates
+ */
+export function getCanvasCoords(event) {
+  const canvas = event.target;
+  const { top, left } = canvas.getBoundingClientRect();
+
+  let x = event.pageX - left;
+  let y = event.pageY - top;
+
+  return { x, y };
+}
+
+function Canvas({ draw, ...props }) {
   const ref = useRef();
 
   useEffect(() => {
@@ -19,17 +36,7 @@ function Canvas({ draw, onClick, ...props }) {
     };
   });
 
-  const handleClick = (e) => {
-    const canvas = ref.current;
-    const { top, left } = canvas.getBoundingClientRect();
-
-    let x = e.pageX - left;
-    let y = e.pageY - top;
-
-    onClick(e, { x, y });
-  };
-
-  return <canvas {...props} ref={ref} onClick={handleClick} />;
+  return <canvas {...props} ref={ref} />;
 }
 
 export default Canvas;
--- src/components/inputs/PositionInput/CartesianPlane.js
+++ src/components/inputs/PositionInput/CartesianPlane.js
@@ -1,4 +1,18 @@
-import Canvas from './Canvas';
+import Canvas, { getCanvasCoords } from './Canvas';
+
+//#region helper functions
+
+// cartesian plane coord -> canvas coord
+const toCanvasCoords = (x, axis, canvasSize) => {
+  const { min, max } = axis;
+  return (x / (max - min)) * canvasSize;
+};
+
+// canvas coord -> cartesian plane coord
+const toPositionCoords = (x, axis, canvasSize) => {
+  const { min, max } = axis;
+  return (x / canvasSize) * (max - min);
+};
 
 function resizeCanvasSquare(canvas) {
   const { width } = canvas.getBoundingClientRect();
@@ -14,113 +28,111 @@ function resizeCanvasSquare(canvas) {
   return width;
 }
 
-// scale a dimension to the axis
-const toCanvasCoords = (x, axis, canvasSize) => {
-  const { min, max } = axis;
-  return (x / (max - min)) * canvasSize;
+//#endregion
+
+//#region rendering functions
+
+const drawMarks = (ctx, size, axis) => {
+  // helper functions
+  const strokeLine = (x1, y1, x2, y2) => {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+  };
+
+  // varaibles
+  const lines = 20;
+
+  // reset canvas
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, size, size);
+
+  // lines
+  ctx.strokeStyle = '#ccc';
+  for (let i = 0; i < lines; i++) {
+    const n = i * (size / lines);
+    strokeLine(n, 0, n, size); // vertical
+    strokeLine(0, n, size, n); // horizontal
+  }
+
+  // axis
+  ctx.strokeStyle = '#777';
+  ctx.lineWidth = 2;
+  strokeLine(size / 2, 0, size / 2, size);
+  strokeLine(0, size / 2, size, size / 2);
+
+  // begin: lables
+  ctx.fillStyle = '#000';
+  ctx.font = `16px monospace`;
+
+  const lm = 4; // label margin
+  const s2 = size / 2; // half size
+
+  // label axis names
+  ctx.textAlign = 'start';
+  ctx.textBaseline = 'top';
+  ctx.fillText(axis.vertical.name, s2 + lm, 0 + lm);
+
+  ctx.textAlign = 'end';
+  ctx.textBaseline = 'bottom';
+  ctx.fillText(axis.horizontal.name, size - lm, s2 - lm);
+
+  // label axis extremes
+  ctx.textAlign = 'start';
+  ctx.textBaseline = 'top';
+  ctx.fillText(axis.horizontal.min, 0 + lm, s2 + lm);
+
+  ctx.textAlign = 'end';
+  ctx.textBaseline = 'top';
+  ctx.fillText(axis.horizontal.max, size - lm, s2 + lm);
+
+  ctx.textAlign = 'end';
+  ctx.textBaseline = 'top';
+  ctx.fillText(axis.vertical.max, s2 - lm, 0 + lm);
+
+  ctx.textAlign = 'end';
+  ctx.textBaseline = 'bottom';
+  ctx.fillText(axis.vertical.min, s2 - lm, size - lm);
 };
 
-const toPositionCoords = (x, axis, canvasSize) => {
-  const { min, max } = axis;
-  return (x / canvasSize) * (max - min);
+const drawPos = (ctx, size, axis, pos) => {
+  if (!pos) return;
+
+  const { horizontal, vertical } = pos;
+
+  const x = toCanvasCoords(horizontal, axis.horizontal, size);
+  const y = toCanvasCoords(vertical, axis.vertical, size);
+
+  ctx.save();
+  ctx.translate(size / 2, size / 2);
+  ctx.scale(1, -1);
+
+  ctx.fillStyle = 'red';
+  ctx.beginPath();
+  ctx.arc(x, y, 5, 0, 2 * Math.PI);
+  ctx.fill();
+
+  ctx.restore();
 };
+
+//#endregion
 
 function CartesianPlane({ axis, pos, onClick }) {
-  const drawMarks = ({ context: ctx, size }) => {
-    // helper functions
-    const strokeLine = (x1, y1, x2, y2) => {
-      ctx.beginPath();
-      ctx.moveTo(x1, y1);
-      ctx.lineTo(x2, y2);
-      ctx.stroke();
-    };
-
-    // varaibles
-    const lines = 20;
-
-    // reset canvas
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, size, size);
-
-    // lines
-    ctx.strokeStyle = '#ccc';
-    for (let i = 0; i < lines; i++) {
-      const n = i * (size / lines);
-      strokeLine(n, 0, n, size); // vertical
-      strokeLine(0, n, size, n); // horizontal
-    }
-
-    // axis
-    ctx.strokeStyle = '#777';
-    ctx.lineWidth = 2;
-    strokeLine(size / 2, 0, size / 2, size);
-    strokeLine(0, size / 2, size, size / 2);
-
-    // begin: lables
-    ctx.fillStyle = '#000';
-    ctx.font = `16px monospace`;
-
-    const lm = 4; // label margin
-    const s2 = size / 2; // half size
-
-    // label axis names
-    ctx.textAlign = 'start';
-    ctx.textBaseline = 'top';
-    ctx.fillText(axis.vertical.name, s2 + lm, 0 + lm);
-
-    ctx.textAlign = 'end';
-    ctx.textBaseline = 'bottom';
-    ctx.fillText(axis.horizontal.name, size - lm, s2 - lm);
-
-    // label axis extremes
-    ctx.textAlign = 'start';
-    ctx.textBaseline = 'top';
-    ctx.fillText(axis.horizontal.min, 0 + lm, s2 + lm);
-
-    ctx.textAlign = 'end';
-    ctx.textBaseline = 'top';
-    ctx.fillText(axis.horizontal.max, size - lm, s2 + lm);
-
-    ctx.textAlign = 'end';
-    ctx.textBaseline = 'top';
-    ctx.fillText(axis.vertical.max, s2 - lm, 0 + lm);
-
-    ctx.textAlign = 'end';
-    ctx.textBaseline = 'bottom';
-    ctx.fillText(axis.vertical.min, s2 - lm, size - lm);
-  };
-
-  const drawPos = ({ context: ctx, size }) => {
-    if (!pos) return;
-
-    const { horizontal, vertical } = pos;
-
-    const x = toCanvasCoords(horizontal, axis.horizontal, size);
-    const y = toCanvasCoords(vertical, axis.vertical, size);
-
-    ctx.save();
-    ctx.translate(size / 2, size / 2);
-    ctx.scale(1, -1);
-
-    ctx.fillStyle = 'red';
-    ctx.beginPath();
-    ctx.arc(x, y, 5, 0, 2 * Math.PI);
-    ctx.fill();
-
-    ctx.restore();
-  };
-
-  const draw = ({ context, ...params }) => {
+  const draw = ({ context }) => {
     const size = resizeCanvasSquare(context.canvas);
-    drawMarks({ size, context, ...params });
-    drawPos({ size, context, ...params });
+    drawMarks(context, size, axis);
+    drawPos(context, size, axis, pos);
   };
 
-  const handleClick = (event, canvasPos) => {
-    const size = event.target.width;
+  // takes a MouseEvent fire from a <canvas> and converts to the coordiantes used by the <CartesianPlane />
+  const handleClick = (event) => {
+    const canvas = event.target;
+    const size = canvas.width;
 
-    const x = toPositionCoords(canvasPos.x - size / 2, axis.horizontal, size);
-    const y = toPositionCoords(-(canvasPos.y - size / 2), axis.vertical, size);
+    const coords = getCanvasCoords(event);
+    const x = toPositionCoords(coords.x - size / 2, axis.horizontal, size);
+    const y = toPositionCoords(-(coords.y - size / 2), axis.vertical, size);
 
     const pos = {
       horizontal: Math.round(x),
